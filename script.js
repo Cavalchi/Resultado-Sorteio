e77fd6f25493766b1d1d26d0fbc5c9e4de19431d
Cavalchi <jpcavalchikar@hotmail.com>
2023-04-16 02:31:39 -0300

    Resultado

---
+++ script.js
@@ -0,0 +1,8 @@
+var teste = fetch('https://parseapi.back4app.com/classes/Person', {
+      method: 'GET',
+    headers: {
+      'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J',
+      'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW',
+      'Content-Type': 'application/json'
+    }
+  })  
