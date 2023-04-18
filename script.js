7cd794508a6616e81fcda54dd3f4ead910ba6cf2
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2023-04-18 00:42:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,9 @@ let email = formDetail.email;
 fetch('https://parseapi.back4app.com/classes/Person', {
   method: 'GET',
   headers: {
-   Parse.initialize('YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J', 'au7MMDVyPkYupjM9FuYI8niBmZlyggsJzq1mPEUE', 'yvd92zfzIdacsPzUl9xUPUY4mcACLHCF8kIui5rn');
+ 'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J',
+    'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW',
+    'X-Parse-Master-Key': 'yvd92zfzIdacsPzUl9xUPUY4mcACLHCF8kIui5rn'
 Parse.serverURL = 'https://parseapi.back4app.com/classes/Person';
     'Content-Type': 'application/json'
   }
