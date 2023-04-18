ffa7b0c6bc4155d39b9f2f1f11aea121099fccbc
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2023-04-18 12:29:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('Página carregada');
 
-// Selecionando os elementos HTML onde as informações serão exibidas
-const userEmailElement = document.querySelector('#email');
-const userNameElement = document.querySelector('#nome');
-const userLastnameElement = document.querySelector('#sobrenome');
-const userPhoneElement = document.querySelector('#telefone');
-const userNumberElement = document.querySelector('#numero');
+  // Selecionando os elementos HTML onde as informações serão exibidas
+  const userEmailElement = document.querySelector('#email');
+  const userNameElement = document.querySelector('#nome');
+  const userLastnameElement = document.querySelector('#sobrenome');
+  const userPhoneElement = document.querySelector('#telefone');
+  const userNumberElement = document.querySelector('#numero');
 
-// Selecionando o botão de sorteio
-const btnSorteio = document.querySelector('#btn-submit');
+  // Selecionando o botão de sorteio
+  const btnSorteio = document.querySelector('#btn-submit');
 
-// Adicionando o evento de clique ao botão de sorteio
-btnSorteio.addEventListener('click', function(event) {
-  event.preventDefault(); // evita que a página seja recarregada
+  // Adicionando o evento de clique ao botão de sorteio
+  btnSorteio.addEventListener('click', function(event) {
+    console.log('Botão de sorteio clicado');
+    event.preventDefault(); // evita que a página seja recarregada
 
-  // Fazendo a requisição para obter o número total de linhas na tabela "Person"
-  fetch('https://parseapi.back4app.com/classes/Person?count=1&limit=0', {
-    headers: {
-      'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J', 
-      'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    const totalRows = data.count;
-    // Gerando um número aleatório entre 0 e totalRows
-    const randomRow = Math.floor(Math.random() * totalRows);
-
-    // Fazendo a requisição para obter a linha aleatória da tabela "Person"
-    fetch(`https://parseapi.back4app.com/classes/Person?skip=${randomRow}&limit=1`, {
+    // Fazendo a requisição para obter o número total de linhas na tabela "Person"
+    fetch('https://parseapi.back4app.com/classes/Person?count=1&limit=0', {
       headers: {
         'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J', 
         'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW'
@@ -36,16 +25,30 @@ btnSorteio.addEventListener('click', function(event) {
     })
     .then(response => response.json())
     .then(data => {
-      // Atualizando o conteúdo dos elementos HTML com os dados puxados
-      const user = data.results[0];
-      userEmailElement.textContent = `Email: ${user.email}`;
-      userNameElement.textContent = `Nome: ${user.name}`;
-      userLastnameElement.textContent = `Sobrenome: ${user.lastname}`;
-      userPhoneElement.textContent = `Telefone: ${user.phone}`;
-      userNumberElement.textContent = `Número do sorteio: ${user.number}`;
-    })
-    .catch(error => console.error(error));
-  })
-  .catch(error => console.error(error));
-});
+      console.log('Número total de linhas obtido:', data.count);
+      const totalRows = data.count;
+      // Gerando um número aleatório entre 0 e totalRows
+      const randomRow = Math.floor(Math.random() * totalRows);
+      console.log('Linha aleatória gerada:', randomRow);
+
+      // Fazendo a requisição para obter a linha aleatória da tabela "Person"
+      fetch(`https://parseapi.back4app.com/classes/Person?skip=${randomRow}&limit=1`, {
+        headers: {
+          'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J', 
+          'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Dados do usuário obtidos:', data.results[0]);
+        // Atualizando o conteúdo dos elementos HTML com os dados puxados
+        const user = data.results[0];
+        userEmailElement.textContent = `Email: ${user.email}`;
+        userNameElement.textContent = `Nome: ${user.name}`;
+        userLastnameElement.textContent = `Sobrenome: ${user.lastname}`;
+        userPhoneElement.textContent = `Telefone: ${user.phone}`;
+        userNumberElement.textContent = `Número: ${user.number}`;
+      });
+    });
+  });
 });
