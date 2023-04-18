92a10fe47bae1b305b65f8ae65f6b2f73b6adbd4
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2023-04-18 10:03:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,99 +8,100 @@ class Validator {
       'data-required',
       'data-equal',
       'data-password-validate',
-    ]
+    ];
   }
 
   validate(form) {
-    let currentValidations = document.querySelectorAll('form .error-validation');
-    if(currentValidations.length) {
+    let currentValidations = form.querySelectorAll('.error-validation');
+    if (currentValidations.length) {
       this.cleanValidations(currentValidations);
     }
     let inputs = form.getElementsByTagName('input');
     let inputsArray = [...inputs];
-    inputsArray.forEach(function(input, obj) {
-      for(let i = 0; this.validations.length > i; i++) {
-        if(input.getAttribute(this.validations[i]) != null) {
-          let method = this.validations[i].replace("data-", "").replace("-", "");
-          let value = input.getAttribute(this.validations[i])
-          this[method](input,value);
+    inputsArray.forEach((input) => {
+      for (let i = 0; i < this.validations.length; i++) {
+        if (input.getAttribute(this.validations[i]) !== null) {
+          let method = this.validations[i].replace('data-', '').replace('-', '');
+          let value = input.getAttribute(this.validations[i]);
+          this[method](input, value);
         }
       }
-    }, this);
+    });
   }
 
-  minlength(input, minValue) {
+  minLength(input, minValue) {
     let inputLength = input.value.length;
     let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
-    if(inputLength < minValue) {
+    if (inputLength < minValue) {
       this.printMessage(input, errorMessage);
     }
   }
 
-  maxlength(input, maxValue) {
+  maxLength(input, maxValue) {
     let inputLength = input.value.length;
-    let errorMessage = `O campo precisa ter menos que ${maxValue} caracteres`;
-    if(inputLength > maxValue) {
+    let errorMessage = `O campo precisa ter no máximo ${maxValue} caracteres`;
+    if (inputLength > maxValue) {
       this.printMessage(input, errorMessage);
     }
   }
 
-  onlyletters(input) {
-    let re = /^[A-Za-z]+$/;;
+  onlyLetters(input) {
+    let re = /^[A-Za-z]+$/;
     let inputValue = input.value;
     let errorMessage = `Este campo não aceita números nem caracteres especiais`;
-    if(!re.test(inputValue)) {
+    if (!re.test(inputValue)) {
       this.printMessage(input, errorMessage);
     }
   }
 
-  emailvalidate(input) {
+  emailValidate(input) {
     let re = /\S+@\S+\.\S+/;
     let email = input.value;
     let errorMessage = `Insira um e-mail no padrão example@outlook.com`;
-    if(!re.test(email)) {
+    if (!re.test(email)) {
       this.printMessage(input, errorMessage);
     }
+    import Parse from 'parse/node';
   }
 
   equal(input, inputName) {
     let inputToCompare = document.getElementsByName(inputName)[0];
-    let errorMessage = `Este campo precisa estar igual ao ${inputName}`;
-    if(input.value != inputToCompare.value) {
+    let errorMessage = `Este campo precisa estar igual ao campo ${inputName}`;
+    if (input.value !== inputToCompare.value) {
       this.printMessage(input, errorMessage);
     }
   }
 
   required(input) {
-  let inputValue = input.value;
-    if(inputValue === '') {
+    let inputValue = input.value;
+    if (inputValue === '') {
       let errorMessage = `Este campo é obrigatório`;
       this.printMessage(input, errorMessage);
     }
   }
 
-  passwordvalidate(input) {
-    let charArr = input.value.split("");
+  passwordValidate(input) {
+    let charArr = input.value.split('');
     let uppercases = 0;
     let numbers = 0;
-    for(let i = 0; charArr.length > i; i++) {
-      if(charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))) {
+    for (let i = 0; i < charArr.length; i++) {
+      if (charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))) {
         uppercases++;
-      } else if(!isNaN(parseInt(charArr[i]))) {
+      } else if (!isNaN(parseInt(charArr[i]))) {
         numbers++;
       }
     }
-    if(uppercases === 0 || numbers === 0) {
-      let errorMessage = `A senha precisa um caractere maiúsculo e um número`;
-
+    if (uppercases === 0 || numbers === 0) {
+      let errorMessage = `A senha precisa ter pelo menos um caractere maiúsculo e um número`;
       this.printMessage(input, errorMessage);
     }
   }
 
   printMessage(input, msg) {
-      let errorsQty = input.parentNode.querySelector('.error-validation');
-      if(errorsQty === null) {
-        let template = document.querySelector('.error-validation').cloneNode(true);
+    let errorsQty = input.parentNode.querySelector('.error-validation');
+    if (errorsQty === null) {
+      let template
+ = document.querySelector('.error-validation').cloneNode(true);
         template.textContent = msg;
         let inputParent = input.parentNode;
         template.classList.remove('template');
@@ -126,11 +127,12 @@ let email = formDetail.email;
 
 fetch('https://parseapi.back4app.com/classes/Person', {
   method: 'GET',
-  headers: {
+ headers: {
+  'Content-Type': 'application/json',
  'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J',
     'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW',
-    'X-Parse-Master-Key': 'yvd92zfzIdacsPzUl9xUPUY4mcACLHCF8kIui5rn'
-    'Content-Type': 'application/json'
+    
+
   }
 })
 .then(resposta => resposta.text())
@@ -143,8 +145,9 @@ fetch('https://parseapi.back4app.com/classes/Person', {
       if (res.email == email) {
         number = res.number;
       } else {
-        existe = false;
-        const Query = Parse.Object.extend('Person');
+       let existe = false;
+let count = resultadoArr.length;
+const Query = Parse.Object.extend('Person');
 const query = new Parse.Query(Query);
 query.limit(1);
 query.skip(Math.floor(Math.random() * count));
@@ -153,8 +156,9 @@ query.first().then((result) => {
 }).catch((error) => {
   console.error(error);
 });
+
       }
-    });
+    })
 .then(resposta => resposta.text())
 .then(result => {
   setTimeout(() => {
